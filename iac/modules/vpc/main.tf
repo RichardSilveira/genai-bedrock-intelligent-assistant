dcde39a3462151d48160e7c285a2623243705340
Richard Silveira <richardleecba@gmail.com>
2025-05-28 09:59:27 -0400

    refactor: enable 3 az

--- iac/modules/vpc/main.tf
+++ iac/modules/vpc/main.tf
@@ -52,6 +52,17 @@ resource "aws_subnet" "public_2" {
   tags = merge(var.tags, { Name = "${var.name}-public-subnet-2" })
 }
 
+resource "aws_subnet" "public_3" {
+  count             = var.public_subnet_3_cidr != null ? 1 : 0
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.public_subnet_3_cidr
+  availability_zone = var.public_subnet_3_az
+
+  map_public_ip_on_launch = false
+
+  tags = merge(var.tags, { Name = "${var.name}-public-subnet-3" })
+}
+
 # --------------------------------------------------
 # Private Subnets
 # --------------------------------------------------
@@ -76,6 +87,17 @@ resource "aws_subnet" "private_2" {
   tags = merge(var.tags, { Name = "${var.name}-private-subnet-2" })
 }
 
+resource "aws_subnet" "private_3" {
+  count             = var.private_subnet_3_cidr != null ? 1 : 0
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnet_3_cidr
+  availability_zone = var.private_subnet_3_az
+
+  map_public_ip_on_launch = false
+
+  tags = merge(var.tags, { Name = "${var.name}-private-subnet-3" })
+}
+
 # --------------------------------------------------
 # NAT Gateways
 # --------------------------------------------------
@@ -89,6 +111,7 @@ resource "aws_eip" "nat_1" {
 }
 
 resource "aws_eip" "nat_2" {
+  count  = var.create_second_nat ? 1 : 0
   domain = "vpc"
 
   tags = merge(var.tags, { Name = "${var.name}-nat-eip-2" })
@@ -104,7 +127,8 @@ resource "aws_nat_gateway" "nat_1" {
 }
 
 resource "aws_nat_gateway" "nat_2" {
-  allocation_id = aws_eip.nat_2.id
+  count         = var.create_second_nat ? 1 : 0
+  allocation_id = aws_eip.nat_2[0].id
   subnet_id     = aws_subnet.public_2.id
 
   tags = merge(var.tags, { Name = "${var.name}-nat-gateway-2" })
